e21b07f52da611f2efd17c6b3fe01c9f82ac5109
Rezky Yuranda <yurandarezky@atmaluhur.ac.id>
2025-02-03 10:21:45 +0700

    edit: many

--- users/default.nix
+++ users/default.nix
@@ -8,14 +8,48 @@
 
   home.stateVersion = "24.11"; # Please read the comment before changing.
   
+  home.packages = with pkgs;[
+    tela-circle-icon-theme
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.dash-to-dock  
+    gnomeExtensions.vitals
+    gnomeExtensions.caffeine
+  ];
+
   programs.git = {
     enable = true;
     userEmail = "yurandarezky@atmaluhur.ac.id";
     userName = "Rezky Yuranda";
   };
 
-  home.packages = [
-  ];
+  gtk = {
+    enable = true;
+    iconTeheme = {
+      package = pkgs.tela-circle-icon-theme;
+      name = "Tela-circle-icon-theme";
+    };
+  };
+
+  # Dconf
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/mutter" = {
+        experimental-features = [ "scale-monitor-framebuffer" ];
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          caffeine.extensionUuid
+          dash-to-dock.extensionUuid
+          vitals.extensionUuid 
+        ];
+      };
+    };
+  };
+
 
   home.file = {
   };
